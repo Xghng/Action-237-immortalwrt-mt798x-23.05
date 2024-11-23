d18d19e565128afa97fa7975389719a1a326efb5
Xghng <135593952+Xghng@users.noreply.github.com>
2024-11-23 17:27:39 +0800

    Update diy-part2-6-1-hp.sh

--- diy-part2-6-1-hp.sh
+++ diy-part2-6-1-hp.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
+# 修改默认主题
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
