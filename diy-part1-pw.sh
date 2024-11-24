13d9f64bba6b5a27d4fd5d4ec27083e1ec751346
Xghng <135593952+Xghng@users.noreply.github.com>
2024-11-24 17:31:21 +0800

    Update diy-part1-pw.sh

--- diy-part1-pw.sh
+++ diy-part1-pw.sh
@@ -13,7 +13,8 @@
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
+### 添加第三方订阅源
+# sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
