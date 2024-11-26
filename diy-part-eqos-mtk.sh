5f8bf1540d5b92238fc05dd13d70e13a755650f2
Xghng <135593952+Xghng@users.noreply.github.com>
2024-11-26 15:27:55 +0800

    Update diy-part-eqos-mtk.sh

--- diy-part-eqos-mtk.sh
+++ diy-part-eqos-mtk.sh
@@ -20,7 +20,7 @@ sed -i 's/luci-theme-bootstrap/g' feeds/luci/collections/luci/Makefile
 sed -i 's/ImmortalWrt/ImmortalWrt/g' package/base-files/files/bin/config_generate
 
 # Modify filename, add date prefix
-sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +"%Y%m%d")-hp-/1' include/image.mk
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +"%Y%m%d")-eqos-mtk-/1' include/image.mk
 
 # Modify ppp-down, add sleep 3. my source code is change, no need this
 sed -i '$a\\sleep 3' package/network/services/ppp/files/lib/netifd/ppp-down
